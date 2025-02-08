7def30419b4500cb1a550994d47fd616f7d7d47b
Uzi <122372890+UziWasTaken@users.noreply.github.com>
2025-02-08 01:03:11 -0600

    Callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
+import OAuthCallback from './components/OAuthCallback';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/callback" element={<OAuthCallback />} />
         <Route path="/welcome" element={<Welcome />} />
       </Routes>
     </Router>
